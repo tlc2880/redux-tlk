4996980cf90c41c7fed2db650cc4ce098962aff3
tlc2880 <tommylcao@gmail.com>
2023-03-30 09:30:08 -0700

    Add users directory and slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import postsReducer from '../features/posts/postsSlice';
-// import usersReducer from '../features/users/usersSlice';
+import usersReducer from '../features/users/usersSlice';
 //import todosSlice from "../features/todos/todosSlice";
 
 
 export const store = configureStore({
     reducer: {
         posts: postsReducer,
-        // users: usersReducer,
+        users: usersReducer,
         //todos: todosSlice
     }
 })
---
+++ src/user.Type.ts
@@ -0,0 +1,4 @@
+export default interface userType {
+    id: string,
+    name: string
+  }
